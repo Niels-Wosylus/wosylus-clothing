87a2b1050fc8e115dc8e1126ae36c82a32fe2980
Niels Wosylus <niels.wosylus@gmail.com>
2021-03-10 14:39:01 +0100

    some navigation stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = (props) => (
+  <div>
+    <h1>HATS PAGE {props.match.params.hatId}</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats/:hatId' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
